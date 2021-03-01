d658d45d9e13cd9414b28f1ca2ef04be35902936
Clark <clarkjzhang@gmail.com>
2021-02-28 22:46:54 -0500

    pyodide test

---
+++ docs/posts/pyodide_test/script.js
@@ -0,0 +1,12 @@
+
+var editor = ace.edit("editor");
+editor.getSession().setMode("ace/mode/python");
+editor.setTheme("ace/theme/monokai");
+
+languagePluginLoader.then(() => { $("#output").val("Ready\n>>>"); });
+
+$("#button").click(function() {
+    pyodide.runPythonAsync(editor.getValue())
+        .then(function(output) {$("#output").val($("#output").val() + "\n>>>" + String(output));})
+        .catch(function(err) {$("#output").val($("#output").val() + "\n>>>" + String(err));});
+    });
